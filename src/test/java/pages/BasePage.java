36e2ef9eac926fc146603210859a5e4b5829f68c
Maximiliano Hernan Cordero <mcordero@fusap.com.ar>
2021-08-01 10:22:08 -0300

    create method find, clickElement and write in BasePage

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -1,8 +1,12 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class BasePage {
@@ -25,4 +29,18 @@ public class BasePage {
     public static void navegateTo(String url){
         driver.get(url);
     }
+
+    private WebElement find(String locator){
+        return  wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(locator)));
+    }
+
+    public void clickElement(String locator){
+        find(locator).click();
+    }
+
+    public void write(String locator, String textToWrite){
+        find(locator).clear();
+        find(locator).sendKeys(textToWrite);
+    }
+
 }
